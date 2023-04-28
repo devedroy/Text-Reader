36a2d3dfe086ca2bfe30527e6af8c7ab3b2d94f2
Roy <devedroy.dr@gmail.com>
2023-04-28 15:06:53 +0530

    showing text in textView.

--- app/src/main/java/com/devedroy/textreader/MainActivity.kt
+++ app/src/main/java/com/devedroy/textreader/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.devedroy.textreader
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.devedroy.textreader.databinding.ActivityMainBinding
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val inputStream = resources.openRawResource(R.raw.mytextfile)
+        val reader = BufferedReader(InputStreamReader(inputStream))
+        val stringBuilder = StringBuilder()
+
+        var line: String?
+        try {
+            while (reader.readLine().also { line = it } != null) {
+                stringBuilder.append(line).append("\n")
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            try {
+                inputStream.close()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+
+        binding.tvViewer.text = stringBuilder.toString()
+
     }
 }
