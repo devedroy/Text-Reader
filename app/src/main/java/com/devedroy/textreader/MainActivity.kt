0d274b88ffedbd774f8dbdbfe8f9b6ca9d75eb0f
Roy <devedroy.dr@gmail.com>
2023-04-28 16:52:46 +0530

    taking files from file explorer

--- app/src/main/java/com/devedroy/textreader/MainActivity.kt
+++ app/src/main/java/com/devedroy/textreader/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.devedroy.textreader
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.devedroy.textreader.databinding.ActivityMainBinding
@@ -7,31 +9,50 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val REQUEST_CODE = 123
+    }
+
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val inputStream = resources.openRawResource(R.raw.mytextfile)
-        val reader = BufferedReader(InputStreamReader(inputStream))
-        val stringBuilder = StringBuilder()
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "text/plain"
+        startActivityForResult(intent, REQUEST_CODE)
+    }
 
-        var line: String?
-        try {
-            while (reader.readLine().also { line = it } != null) {
-                stringBuilder.append(line).append("\n")
-            }
-        } catch (e: Exception) {
-            e.printStackTrace()
-        } finally {
-            try {
-                inputStream.close()
-            } catch (e: Exception) {
-                e.printStackTrace()
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            val uri = data?.data
+            if (uri != null) {
+                try {
+                    val inputStream = contentResolver.openInputStream(uri)
+                    val reader = BufferedReader(InputStreamReader(inputStream))
+                    val stringBuilder = StringBuilder()
+
+                    var line: String?
+                    try {
+                        while (reader.readLine().also { line = it } != null) {
+                            stringBuilder.append(line).append("\n")
+                        }
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    } finally {
+                        try {
+                            inputStream?.close()
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                        }
+                    }
+                    binding.wvViewer.loadData(stringBuilder.toString(), "text/html", "UTF-8")
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
             }
         }
-
-        binding.wvViewer.loadData(stringBuilder.toString(), "text/html", "UTF-8")
     }
 }
